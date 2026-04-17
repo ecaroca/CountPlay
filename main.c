3dd2028d9015c02fff3be05ee9f389d4305535b6
Allison <eva0p72@gmail.com>
2026-04-16 23:59:07 -0400

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ main(int argc, char **argv)
     GtkApplication *app;
     int stat;
 
-    app = gtk_application_new("cl.allison.calculadora", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("cl.countplay", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
     stat = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
