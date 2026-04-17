fb4c581e0e5746a9495046517599eb76a5d4fa3d
Live session user <eva0p72@gmail.com>
2026-04-16 23:30:15 -0400

    Agregar código fuente y .gitignore

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <gtk/gtk.h>
+#include "ui.h"
+
+// Archivo principal: inicia la aplicación, es necesario instalar GTK4:
+//       sudo apt install libgtk-4-dev pkg-config
+//Para compilar:
+//       gcc $(pkg-config --cflags gtk4) main.c ui.c -o main $(pkg-config --libs gtk4)
+
+static void
+app_activate(GApplication *app, gpointer user_data)
+{
+    build_ui(GTK_APPLICATION(app));
+}
+
+int
+main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int stat;
+
+    app = gtk_application_new("cl.allison.calculadora", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+    stat = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return stat;
+}
---
+++ ui.h
@@ -0,0 +1,10 @@
+// Cabecera para funciones de UI: declaración de funciones.
+
+#ifndef UI_H
+#define UI_H
+
+#include <gtk/gtk.h>
+
+void build_ui(GtkApplication *app);
+
+#endif
