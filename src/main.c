cf691d2eb6eb6b65d836cfd0e1aaa142a14f831a
ItzTercer <diamirahumada@gmail.com>
2026-04-21 22:09:41 -0400

    Organizar pruebas en front-end-test y restaurar versión básica en src

--- src/main.c
+++ src/main.c
@@ -1,29 +1,66 @@
-#include <gtk/gtk.h>
-#include "ui.h"
+#include <stdio.h>
 
-// Archivo principal: inicia la aplicación, !!es necesario instalar GTK4:
-//       sudo apt install libgtk-4-dev pkg-config
-//Para compilar:
-//       gcc $(pkg-config --cflags gtk4) main.c ui.c -o main $(pkg-config --libs gtk4)
-//Puede que en vs studio code salga error en el "#include <gtk/gtk.h>", si es que gtk4
-//esta correctamente instalado y además la app compila bien: se puede ignorar el error.
+int main(void) {
+    int opcion;
+    double numero1;
+    double numero2;
+    double resultado;
+    int continuar = 1;
 
-static void
-app_activate(GApplication *app, gpointer user_data)
-{
-    build_ui(GTK_APPLICATION(app));
-}
+    while (continuar == 1) {
+        printf("\n=================================\n");
+        printf("     Count Play\n");
+        printf("=================================\n");
+        printf("1. Suma\n");
+        printf("2. Resta\n");
+        printf("3. Multiplicacion\n");
+        printf("4. Division\n");
+        printf("5. Salir\n");
+        printf("Opcion: ");
+        scanf("%d", &opcion);
 
-int
-main(int argc, char **argv)
-{
-    GtkApplication *app;
-    int stat;
+        if (opcion == 5) {
+            printf("Saliendo de la calculadora...\n");
+            break;
+        }
 
-    app = gtk_application_new("cl.countplay", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
-    stat = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+        if (opcion < 1 || opcion > 5) {
+            printf("Opcion no valida.\n");
+            continue;
+        }
 
-    return stat;
+        printf("Ingrese el primer numero: ");
+        scanf("%lf", &numero1);
+
+        printf("Ingrese el segundo numero: ");
+        scanf("%lf", &numero2);
+
+        switch (opcion) {
+            case 1:
+                resultado = numero1 + numero2;
+                printf("Resultado: %.2lf\n", resultado);
+                break;
+
+            case 2:
+                resultado = numero1 - numero2;
+                printf("Resultado: %.2lf\n", resultado);
+                break;
+
+            case 3:
+                resultado = numero1 * numero2;
+                printf("Resultado: %.2lf\n", resultado);
+                break;
+
+            case 4:
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                    printf("Resultado: %.2lf\n", resultado);
+                } else {
+                    printf("Error: no se puede dividir por cero.\n");
+                }
+                break;
+        }
+    }
+
+    return 0;
 }
