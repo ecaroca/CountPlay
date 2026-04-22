62294fe54489d127dfc2171225c9752a31b45814
ItzTercer <diamirahumada@gmail.com>
2026-04-22 12:37:51 -0400

    Estructura de frontend: front-end-test renombrado a src

--- src/calc.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-double sumar(double a, double b);
-double restar(double a, double b);
-double multiplicar(double a, double b);
-double dividir(double a, double b);
-
--- src/main.c
+++ src/main.c
@@ -1,41 +1,29 @@
-#include "calc.h"
+#include <gtk/gtk.h>
+#include "ui.h"
 
-int main(void) {
-    int opcion;
-    double n1, n2, res;
+// Archivo principal: inicia la aplicación, !!es necesario instalar GTK4:
+//       sudo apt install libgtk-4-dev pkg-config
+//Para compilar:
+//       gcc $(pkg-config --cflags gtk4) main.c ui.c -o main $(pkg-config --libs gtk4)
+//Puede que en vs studio code salga error en el "#include <gtk/gtk.h>", si es que gtk4
+//esta correctamente instalado y además la app compila bien: se puede ignorar el error.
 
-    while (1) {
-        printf("\n=================================\n");
-        printf("           Count Play\n");
-        printf("=================================\n");
-        printf("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir\n");
-        printf("Seleccione una opcion: ");
-        scanf("%d", &opcion);
+static void
+app_activate(GApplication *app, gpointer user_data)
+{
+    build_ui(GTK_APPLICATION(app));
+}
 
-        if (opcion == 5) break;
-        
-        if (opcion < 1 || opcion > 5) {
-            printf("Opcion no valida.\n");
-            continue;
-        }
+int
+main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int stat;
 
-        printf("Ingrese primer numero: ");
-        scanf("%lf", &n1);
-        printf("Ingrese segundo numero: ");
-        scanf("%lf", &n2);
+    app = gtk_application_new("cl.countplay", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+    stat = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 
-        if (opcion == 1) res = sumar(n1, n2);
-        else if (opcion == 2) res = restar(n1, n2);
-        else if (opcion == 3) res = multiplicar(n1, n2);
-        else if (opcion == 4) {
-            if (n2 != 0) res = dividir(n1, n2);
-            else {
-                printf("Error: Division por cero.\n");
-                continue;
-            }
-        }
-        
-        printf("Resultado: %.2lf\n", res);
-    }
-    return 0;
+    return stat;
 }
